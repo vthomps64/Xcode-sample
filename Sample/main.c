ee27220075079a1868530da81e068b9ff4870a40
Vic Thompson <Vic@Vics-MacBook-Pro.local>
2013-08-21 20:45:51 -0400

    Initial Commit

---
+++ Sample/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  Sample
+//
+//  Created by Vic Thompson on 2013-08-21.
+//  Copyright (c) 2013 Vic Thompson. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
