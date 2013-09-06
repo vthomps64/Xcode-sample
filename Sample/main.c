d04eed32a2f239c49604b98377d600b98a911bce
vthomps64 <vthomps@gmail.com>
2013-09-05 20:33:57 -0400

    This page intentionally left blank.

--- Sample/main.c
+++ Sample/main.c
@@ -7,12 +7,100 @@
 //
 
 #include <stdio.h>
+#include <stdbool.h>
 
-int main(int argc, const char * argv[])
+int GetArgument (bool isLeft);
+
+int GetArgument (bool isLeft)
 {
+    int     vNum;
+    if (isLeft)
+        printf("Enter operand 1: ");
+    else
+        printf("Enter operand 2: ");
+        scanf("%d", &vNum);
+        fpurge(stdin);
 
-    // insert code here...
-    printf("Hello, World!\n");
-    return 0;
+    return vNum;
+
 }
 
+int main()
+{
+    int     vFirstArg,
+            vSecondArg;
+    char    vOperation;
+    bool    vFinished;
+    
+    // Initialise flag
+    vFinished = false;
+    
+    // Loop until user is done
+    while (vFinished != true)
+    {
+        printf("What operation do you want to do?\n");
+        scanf("%c", &vOperation);
+        fpurge(stdin);
+
+        if (vOperation == '+')
+        {
+            vFirstArg = GetArgument(true);
+            vSecondArg = GetArgument(false);
+
+            printf("\n%d + %d = %d\n",
+                   vFirstArg,
+                   vSecondArg,
+                   vFirstArg+vSecondArg);
+        }
+        else
+
+        if (vOperation == '-')
+        {
+            vFirstArg = GetArgument(true);
+            vSecondArg = GetArgument(false);
+            
+            printf("\n%d - %d = %d\n",
+                   vFirstArg,
+                   vSecondArg,
+                   vFirstArg-vSecondArg);
+        }
+        else
+        
+        if (vOperation == '*')
+        {
+            vFirstArg = GetArgument(true);
+            vSecondArg = GetArgument(false);
+            
+            printf("\n%d x %d = %d\n",
+                   vFirstArg,
+                   vSecondArg,
+                   vFirstArg*vSecondArg);
+        }
+        else
+
+        if (vOperation == '/')
+        {
+            vFirstArg = GetArgument(true);
+            vSecondArg = GetArgument(false);
+            
+            while (vSecondArg == 0)
+            {
+                printf("Invalid value, re-enter operand 2: ");
+                scanf("%d", &vSecondArg);
+                fpurge(stdin);
+            }
+            
+            printf("\n%d ÷ %d = %d\n",
+                   vFirstArg,
+                   vSecondArg,
+                   vFirstArg/vSecondArg);
+        }
+
+        else
+            vFinished = true;
+    }
+
+    printf("Finished.\n");
+
+    return 0;
+}
